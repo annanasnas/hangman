bbfbfca2d1ba61e5fd57eb5ed7d60df31ebddc48
avsyurakshina1 <avsyurakshina@miem.hse.ru>
2022-06-15 13:16:17 +0300

    create: PrintMessage & MessageInTheMiddle & DrawHangman logics

--- main.cpp
+++ main.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
+using namespace std;
 
+string MessageInTheMiddle(string message, int lineLength)
+{
+    for (int i = message.length(); i < lineLength - 2; i++)
+    {
+        if (i % 2 == 0) message = " " + message;
+        else message = message + " ";
+    }
+    return "|" + message + "|";
+}
+
+void PrintMessage(string content, bool printTop = true, bool printBottom = true)
+{
+    string line = "+---------------------------------+";
+    if (printTop) cout << line << endl;
+    cout << MessageInTheMiddle(content, line.length()) << endl;
+    if (printBottom) cout << line << endl;
+}
+void DrawHangman(int guessCount)
+{
+    if (guessCount >= 1)
+        PrintMessage("|", false, false);
+    else
+        PrintMessage("", false, false);
+
+    if (guessCount >= 2)
+        PrintMessage("|", false, false);
+    else
+        PrintMessage("", false, false);
+
+    if (guessCount >= 3)
+        PrintMessage("O", false, false);
+    else
+        PrintMessage("", false, false);
+
+    if (guessCount == 4)
+        PrintMessage("/  ", false, false);
+
+    if (guessCount == 5)
+        PrintMessage("/| ", false, false);
+
+    if (guessCount >= 6)
+        PrintMessage("/|\\", false, false);
+    else
+        PrintMessage("", false, false);
+
+    if (guessCount >= 7)
+        PrintMessage("|", false, false);
+    else
+        PrintMessage("", false, false);
+
+    if (guessCount == 8)
+        PrintMessage("/", false, false);
+
+    if (guessCount >= 9)
+        PrintMessage("/ \\", false, false);
+    else
+        PrintMessage("", false, false);
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    PrintMessage("Hangman", true, false);
+    PrintMessage("by annanasnas", false, true);
+    DrawHangman(5);
+    getchar();
     return 0;
 }
