d32473ab5643510fd00067baad787c85084a3322
annanasnas <avsyurakshina@miem.hse.ru>
2022-06-16 18:07:42 +0300

    first worked version of desktop hangman

--- main.cpp
+++ main.cpp
@@ -1,214 +1,258 @@
-//#include <iostream>
-//#include <fstream>
-//#include <string>
-//#include <vector>
-//#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
 #include <SFML/Graphics.hpp>
-//using namespace std;
-//
-//class Database
-//{
-//    string wordToGuess;
-//    string hint;
-//public:
-//    Database(string filePath)
-//    {
-//        string word;
-//        vector<string> v;
-//        ifstream reader(filePath);
-//        if (reader.is_open())
-//        {
-//            while (std::getline(reader, word)) v.push_back(word);
-//            int randomLine = rand() % v.size();
-//            string wordAndHint = v.at(randomLine);
-//            int index = 0;
-//            for(int i = wordAndHint.length() - 1; i >= 0; --i) {
-//                if (wordAndHint[i] == ' ') {
-//                    index = i;
-//                    break;
-//                }
-//            }
-//            hint = wordAndHint.substr(0, index);
-//            wordToGuess = wordAndHint.erase(0,wordAndHint.find_last_of(' ')+1);
-//            reader.close();
-//        }
-//    }
-//    string& returnWord()
-//    {
-//        return wordToGuess;
-//    }
-//    string& returnHint()
-//    {
-//        return hint;
-//    }
-//};
-//
-//string MessageInTheMiddle(string message, int lineLength)
-//{
-//    for (int i = message.length(); i < lineLength - 2; i++)
-//    {
-//        if (i % 2 == 0) message = " " + message;
-//        else message = message + " ";
-//    }
-//    return "|" + message + "|";
-//}
-//
-//void PrintMessage(string content, bool printTop = true, bool printBottom = true)
-//{
-//    string line = "+---------------------------------+";
-//    if (printTop) cout << line << endl;
-//    cout << MessageInTheMiddle(content, line.length()) << endl;
-//    if (printBottom) cout << line << endl;
-//}
-//
-//void PrintTitle(string title, string author)
-//{
-//    PrintMessage(title, true, false);
-//    PrintMessage("by " + author, false, true);
-//}
-//
-//void DrawHangman(int guessCount)
-//{
-//    if (guessCount >= 1)
-//        PrintMessage("|", false, false);
-//    else
-//        PrintMessage("", false, false);
-//
-//    if (guessCount >= 2)
-//        PrintMessage("|", false, false);
-//    else
-//        PrintMessage("", false, false);
-//
-//    if (guessCount >= 3)
-//        PrintMessage("O", false, false);
-//    else
-//        PrintMessage("", false, false);
-//
-//    if (guessCount == 4)
-//        PrintMessage("/  ", false, false);
-//
-//    if (guessCount == 5)
-//        PrintMessage("/| ", false, false);
-//
-//    if (guessCount >= 6)
-//        PrintMessage("/|\\", false, false);
-//    else
-//        PrintMessage("", false, false);
-//
-//    if (guessCount >= 7)
-//        PrintMessage("|", false, false);
-//    else
-//        PrintMessage("", false, false);
-//
-//    if (guessCount == 8)
-//        PrintMessage("/  ", false, false);
-//
-//    if (guessCount >= 9)
-//        PrintMessage("/ \\", false, false);
-//    else
-//        PrintMessage("", false, false);
-//}
-//
-//void PrintAvailableLetters(string wordFromUser, char from, char to)
-//{
-//    string availableLetters;
-//    for (char i = from; i <= to; i++)
-//    {
-//        if (wordFromUser.find(i) == string::npos)
-//        {
-//            availableLetters += i;
-//            availableLetters += " ";
-//        }
-//        else availableLetters += "  ";
-//    }
-//    PrintMessage(availableLetters, false, false);
-//}
-//
-//void PrintLetters(string wordFromUser)
-//{
-//    PrintMessage("Available letters");
-//    PrintAvailableLetters(wordFromUser, 'A', 'M');
-//    PrintAvailableLetters(wordFromUser,'N', 'Z');
-//}
-//
-//bool PrintWordAndCheckWin(string wordToGuess, string wordFromUser, string hint)
-//{
-//    bool won = true;
-//    string wordDisplayed;
-//    for (int i = 0; i < wordToGuess.length(); i++)
-//    {
-//        if (wordFromUser.find(wordToGuess[i]) == string::npos)
-//        {
-//            won = false;
-//            wordDisplayed += "_ ";
-//        }
-//        else
-//        {
-//            wordDisplayed += wordToGuess[i];
-//            wordDisplayed += " ";
-//        }
-//    }
-//    PrintMessage("Hint", true, false);
-//    PrintMessage(hint, false, true);
-//    PrintMessage(wordDisplayed, false);
-//    return won;
-//}
-//
-//int TriesLeft(string wordToGuess, string wordFromUser)
-//{
-//    int error = 0;
-//    for (int i = 0; i < wordFromUser.length(); i++)
-//    {
-//        if (wordToGuess.find(wordFromUser[i]) == string::npos) error++;
-//    }
-//    return error;
-//}
-//
-//int main() {
-//    srand(time(0));
-//    string title = "Hangman";
-//    string author = "annanasnas";
-//    Database wordAndHint = Database( "/Users/anastasia/CLionProjects/hangman/words.txt");
-//    int tries = 0;
-//    bool won = false;
-//    string wordFromUser;
-//    do {
-//        system("clear");
-//        PrintTitle(title, author);
-//        DrawHangman(tries);
-//        PrintLetters(wordFromUser);
-//        if (PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint())) won = true;
-//        if (won) break;
-//        char letterFromUser;
-//        cout << ">";
-//        cin >> letterFromUser;
-//        letterFromUser = toupper(letterFromUser);
-//        if (wordFromUser.find(letterFromUser) == string::npos) wordFromUser += letterFromUser;
-//        tries = TriesLeft(wordAndHint.returnWord(), wordFromUser);
-//    } while(tries < 10);
-//    if (won) PrintMessage("You won!", false);
-//    else PrintMessage("Game over", false);
-//    getchar();
-//    return 0;
-//}
+using namespace std;
+
+class Database
+{
+    string wordToGuess;
+    string hint;
+public:
+    Database(string filePath)
+    {
+        string word;
+        vector<string> v;
+        ifstream reader(filePath);
+        if (reader.is_open())
+        {
+            while (std::getline(reader, word)) v.push_back(word);
+            int randomLine = rand() % v.size();
+            string wordAndHint = v.at(randomLine);
+            int index = 0;
+            for(int i = wordAndHint.length() - 1; i >= 0; --i) {
+                if (wordAndHint[i] == ' ') {
+                    index = i;
+                    break;
+                }
+            }
+            hint = wordAndHint.substr(0, index);
+            wordToGuess = wordAndHint.erase(0,wordAndHint.find_last_of(' ')+1);
+            reader.close();
+        }
+    }
+    string& returnWord()
+    {
+        return wordToGuess;
+    }
+    string& returnHint()
+    {
+        return hint;
+    }
+};
+
+
+string MessageInTheMiddle(string message, int lineLength)
+{
+    for (int i = message.length(); i < lineLength - 2; i++)
+    {
+        if (i % 2 == 0) message = " " + message;
+        else message = message + " ";
+    }
+    return "|" + message + "|";
+}
+
+string PrintMessage(string content, bool printTop = true, bool printBottom = true)
+{
+    string result;
+    string line = "+---------------------------------+";
+    if (printTop)
+    {
+        result += line;
+        result += "\n";
+    }
+    result += MessageInTheMiddle(content, line.length());
+    result += "\n";
+    if (printBottom)
+    {
+        result += line;
+        result += "\n";
+    }
+    return result;
+}
+
+string PrintTitle(string title, string author)
+{
+    string result;
+    result += PrintMessage(title, true, false);
+    result += PrintMessage("by " + author, false, true);
+    return result;
+}
+
+string DrawHangman(int guessCount)
+{
+    string result;
+    if (guessCount >= 1)
+        result += PrintMessage("|", false, false);
+    else
+        result += PrintMessage("", false, false);
+
+    if (guessCount >= 2)
+        result += PrintMessage("|", false, false);
+    else
+        result += PrintMessage("", false, false);
+
+    if (guessCount >= 3)
+        result += PrintMessage("O", false, false);
+    else
+        result += PrintMessage("", false, false);
+
+    if (guessCount == 4)
+        result += PrintMessage("/  ", false, false);
+
+    if (guessCount == 5)
+        result += PrintMessage("/| ", false, false);
+
+    if (guessCount >= 6)
+        result += PrintMessage("/|\\", false, false);
+    else
+        result += PrintMessage("", false, false);
+
+    if (guessCount >= 7)
+        result += PrintMessage("|", false, false);
+    else
+        result += PrintMessage("", false, false);
+
+    if (guessCount == 8)
+        result += PrintMessage("/  ", false, false);
+
+    if (guessCount >= 9)
+        result += PrintMessage("/ \\", false, false);
+    else
+        result += PrintMessage("", false, false);
+
+    return result;
+}
+
+string PrintAvailableLetters(string wordFromUser, char from, char to)
+{
+    string result;
+    string availableLetters;
+    for (char i = from; i <= to; i++)
+    {
+        if (wordFromUser.find(i) == string::npos)
+        {
+            availableLetters += i;
+            availableLetters += " ";
+        }
+        else availableLetters += "  ";
+    }
+    result += PrintMessage(availableLetters, false, false);
+    return result;
+}
+
+string PrintLetters(string wordFromUser)
+{
+    string result;
+    result += PrintMessage("Available letters");
+    result += PrintAvailableLetters(wordFromUser, 'A', 'M');
+    result += PrintAvailableLetters(wordFromUser,'N', 'Z');
+    return result;
+}
+
+void PrintWordAndCheckWin(string wordToGuess, string wordFromUser, string hint, bool &won, string &result)
+{
+    bool realWon = true;
+    string wordDisplayed;
+    result = "";
+    for (int i = 0; i < wordToGuess.length(); i++)
+    {
+        if (wordFromUser.find(wordToGuess[i]) == string::npos)
+        {
+            wordDisplayed += "_ ";
+            realWon = false;
+        }
+        else
+        {
+            wordDisplayed += wordToGuess[i];
+            wordDisplayed += " ";
+        }
+    }
+    result += PrintMessage("Hint", true, false);
+    result += PrintMessage(hint, false, true);
+    result += PrintMessage(wordDisplayed, false);
+    won = realWon;
+}
+
+int TriesLeft(string wordToGuess, string wordFromUser)
+{
+    int error = 0;
+    for (int i = 0; i < wordFromUser.length(); i++)
+    {
+        if (wordToGuess.find(wordFromUser[i]) == string::npos) error++;
+    }
+    return error;
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 700), "Hangman");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    srand(time(0));
+    string title = "Hangman";
+    string author = "annanasnas";
+    Database wordAndHint = Database( "/Users/anastasia/CLionProjects/hangman/words.txt");
+    int tries = 0;
+    bool won = false;
+    string result;
+    string wordFromUser;
+
+    sf::RenderWindow window(sf::VideoMode(600, 800), "Hangman");
+    sf::Font font;
+        font.loadFromFile("/Users/anastasia/CLionProjects/hangman/consolas.ttf");
+    sf::Text text("", font, 30);
+    sf::RectangleShape rectangle;
 
     while (window.isOpen())
     {
+        char letterFromUser;
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::TextEntered)
+            {
+                if (event.text.unicode < 128)
+                {
+                    letterFromUser = static_cast<char>(event.text.unicode);
+                    if (wordFromUser.find(letterFromUser) == string::npos) wordFromUser += letterFromUser;
+                    tries = TriesLeft(wordAndHint.returnWord(), wordFromUser);
+                    PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint(), won, result);
+                }
+            }
         }
-
         window.clear();
-        window.draw(shape);
+        text.setString(PrintTitle(title, author));
+        text.setPosition(0, 0);
+        window.draw(text);
+        text.setString(DrawHangman(tries));
+        text.setPosition(0, 100);
+        window.draw(text);
+        text.setString(PrintLetters(wordFromUser));
+        text.setPosition(0, 300);
+        window.draw(text);
+        text.setString(result);
+        text.setPosition(0, 500);
+        window.draw(text);
+        text.setString(">");
+        text.setPosition(0, 700);
+        window.draw(text);
+        text.setString(letterFromUser);
+        text.setPosition(15, 700);
+        window.draw(text);
+        if (tries == 10)
+        {
+            text.setString(PrintMessage("Game over", false));
+            text.setPosition(0, 710);
+            window.draw(text);
+        }
+        if (won)
+        {
+            text.setString(PrintMessage("You won!", false));
+            text.setPosition(0, 710);
+            window.draw(text);
+        }
         window.display();
     }
 
