c476171def2ae3ea0fe535a92e6cb1e073879929
annanasnas <avsyurakshina@miem.hse.ru>
2022-06-18 00:29:24 +0300

    add: documentation

--- main.cpp
+++ main.cpp
@@ -6,7 +6,22 @@
 #include <SFML/Graphics.hpp>
 using namespace std;
 
-
+/*
+ * Документация для класса Database
+ *
+ * @brief Получение слова и его подсказки из файла
+ *
+ * @param filePath - путь к файлу со словами и подсказками
+ *
+ * В конструкторе класса происходит получение случайного
+ * номера строки файла, указанного в filePath. Затем
+ * происходит разделение подсказки и слова по последнему
+ * пробелу в строке, так как формат хранения:
+ * "Какая-то подсказка СЛОВО"
+ * Благодаря методам returnWord и returnHint можно получить
+ * слово и подсказку, соответственно.
+ *
+ * */
 class Database
 {
     string wordToGuess;
@@ -44,7 +59,18 @@ public:
     }
 };
 
-
+/*
+ * Описание функции MessageInTheMiddle
+ *
+ * @brief Равномерно добавить к строке message пробелы слева и справа,
+ * пока не будет достигнута (lineLength - 2) длина. В конце добавить
+ * слева и справа "|"
+ *
+ * @param message - строка, которая будет оцентрирована с помощью пробелов
+ * @param lineLength - длина нового сообщения с message в центре
+ * @return строку вида: "|  message  |",
+ *
+ */
 string MessageInTheMiddle(string message, int lineLength)
 {
     for (int i = message.length(); i < lineLength - 2; i++)
@@ -55,6 +81,18 @@ string MessageInTheMiddle(string message, int lineLength)
     return "|" + message + "|";
 }
 
+/*
+ * Описание функции PrintMessage
+ *
+ * @brief Отрисовка какого-либо контента в игре
+ *
+ * @param content - ЧТО будет отрисовано
+ * @param printTop - если true, то будет отрисована верхняя горизонтальная линия
+ * @param printBottom - если true, то будет отрисована нижняя горизонтальная линия
+ * @return оцентрированный контент с/без верхней и/или нижней линии,
+ * в зависимости от значений printTop и PrintBottom
+ *
+ */
 string PrintMessage(string content, bool printTop = true, bool printBottom = true)
 {
     string result;
@@ -74,7 +112,18 @@ string PrintMessage(string content, bool printTop = true, bool printBottom = tru
     return result;
 }
 
-string PrintTitle(string title, string author)
+/*
+ * Описание функции PrintTitle
+ *
+ * @brief Отрисовка названия и псевдонима автора игры
+ *
+ * @param title - название игры (Hangman)
+ * @param author - псевдоним автора (annanasnas)
+ * @return две строки с title и author по центру
+ * и с верхней и нижней горизонтальной линией
+ *
+ */
+string PrintTitle(string title = "Hangman", string author = "annanasnas")
 {
     string result;
     result += PrintMessage(title, true, false);
@@ -82,6 +131,16 @@ string PrintTitle(string title, string author)
     return result;
 }
 
+/*
+ * Описание функции DrawHangman
+ *
+ * @brief В зависимости от числа неправильных попыток,
+ * будет отрисовываться сам hang man
+ *
+ * @param guessCount - число неправильных попыток
+ * @return строка с нарисованным hang man
+ *
+ */
 string DrawHangman(int guessCount)
 {
     string result;
@@ -127,6 +186,18 @@ string DrawHangman(int guessCount)
     return result;
 }
 
+/*
+ * Описание функции AvailableLetters
+ *
+ * @brief Отображение неиспользованных игроком букв
+ *
+ * @param wordFromUser - использованные игроком буквы
+ * @param from - с какой буквы отображать
+ * @param to - на какой букве остановить отображение
+ * @return строку с буквами от from до to,
+ * кроме тех, что есть в wordFromUser
+ *
+ */
 string PrintAvailableLetters(string wordFromUser, char from, char to)
 {
     string result;
@@ -144,6 +215,17 @@ string PrintAvailableLetters(string wordFromUser, char from, char to)
     return result;
 }
 
+/*
+ * Описание функции PrintLetters
+ *
+ * @brief Отображение букв в английском алфавите,
+ * которые доступны игроку для ввода
+ *
+ * @param wordFromUser - использованные игроком буквы
+ * @return английский алфавит в виде двух оцентрированных
+ * строк без букв, которые есть в wordFromUser
+ *
+ */
 string PrintLetters(string wordFromUser)
 {
     string result;
@@ -153,6 +235,29 @@ string PrintLetters(string wordFromUser)
     return result;
 }
 
+/*
+ * Описание функции PrintWordAndCheckWin
+ *
+ * @brief
+ * 1) Отображение текущих отгаданных игроком
+ * букв и подсказки к загаданному слову.
+ * 2) Проверка на то, выиграл ли игрок
+ *
+ * @param wordToGuess - загаданное слово
+ * @param wordFromUser - введенные игроком буквы
+ * @param hint - подсказка к загаданному слову
+ * @param won - если true, то игрок выиграл
+ * @param result - отображаемое сообщение с
+ * отгаданными буквами и подсказкой
+ *
+ * Проходимся циклом по каждой букве загаднного слова.
+ * Если в буквах, введенных игроком, не находится буквы
+ * из загаданного слова, переменная won становится равной
+ * false, а к результату добавляется "_",
+ * В противном случае, к результату добавляется правильно
+ * отгаданная буква
+ *
+ */
 void PrintWordAndCheckWin(string wordToGuess, string wordFromUser, string hint, bool &won, string &result)
 {
     bool realWon = true;
@@ -177,6 +282,20 @@ void PrintWordAndCheckWin(string wordToGuess, string wordFromUser, string hint, 
     won = realWon;
 }
 
+/*
+ * Описание функции TriesLeft
+ *
+ * @brief Подсчет числа попыток, сделанных игроком
+ *
+ * @param wordToGuess - загаданное слово
+ * @param wordFromUser - введенные игроком буквы
+ * @return число попыток, сделанных игроком
+ *
+ * Проходимся циклом по буквам, введенным игроком.
+ * Если найдется буква, отсутствующая в загаданном слове,
+ * игроку засчитывается +1 попытка.
+ *
+ */
 int TriesLeft(string wordToGuess, string wordFromUser)
 {
     int error = 0;
@@ -187,60 +306,89 @@ int TriesLeft(string wordToGuess, string wordFromUser)
     return error;
 }
 
-void StartNewGame(int &tries, bool &won, string &result, string &wordFromUser, Database &wordAndHint)
+/*
+ * Описание функции StartNewGame
+ *
+ * @brief старт новой игры
+ *
+ * @param tries - число попыток, сделанных пользователем
+ * @param won - выиграл (true) / не выиграл (false)
+ * @param UsersWordAndHint - строка с введенными игроком буквами и подсказкой
+ * @param wordFromUser - введенные игроком буквы
+ * @param wordAndHint - загаданное слово и подсказка
+ *
+ * Изменяем значения всех переменных как в начале игры
+ *
+ */
+void StartNewGame(int &tries, bool &won, string &UsersWordAndHint, string &wordFromUser, Database &wordAndHint)
 {
-    wordAndHint = Database( "/Users/anastasia/CLionProjects/hangman/words.txt");
+    wordAndHint = Database( "../words.txt");
     tries = 0;
     won = false;
-    result = "";
+    UsersWordAndHint = "";
     wordFromUser = "";
-    PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint(), won, result);
+    PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint(), won, UsersWordAndHint);
 }
 
+/*
+ * Описание функции main
+ *
+ * @brief основная логика игры
+ *
+ */
 int main()
 {
     srand(time(0));
     string title = "Hangman";
     string author = "annanasnas";
-    Database wordAndHint = Database( "../words.txt");
+
+    // инициализация основных для игры переменных
+    Database wordAndHint;
     int tries = 0;
     bool won = false;
-    string result;
+    string UsersWordAndHint;
     string wordFromUser;
 
+    // подготовка интерфейса игры
     sf::RenderWindow window(sf::VideoMode(600, 780), "Hangman", sf::Style::Close | sf::Style::Titlebar);
     sf::Font font;
         font.loadFromFile("../consolas.ttf");
     sf::Text text("", font, 30);
 
-    PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint(), won, result);
+    // отображение подсказки и числа букв, что нужно ввести пользователю
+    PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint(), won, UsersWordAndHint);
 
     while (window.isOpen())
     {
         char letterFromUser;
         sf::Event event;
+        // обработчик событий
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
             if (event.type == sf::Event::TextEntered)
             {
+                // если введена буква
                 if (event.text.unicode < 128)
                 {
+                    // пока пользователь не выиграл, можно вводить буквы
                     if (!won)
                     {
                         letterFromUser = toupper(static_cast<char>(event.text.unicode));
                         if (wordFromUser.find(letterFromUser) == string::npos) wordFromUser += letterFromUser;
                         tries = TriesLeft(wordAndHint.returnWord(), wordFromUser);
-                        PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint(), won, result);
+                        PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint(), won, UsersWordAndHint);
                     }
                 }
+                // если пользователь выиграл, он должен нажать "enter" и начать новую игру
                 if (event.text.unicode == 10)
                 {
-                    if (won) StartNewGame(tries, won, result, wordFromUser, wordAndHint);
+                    if (won) StartNewGame(tries, won, UsersWordAndHint, wordFromUser, wordAndHint);
                 }
             }
         }
+        // отрисовка основных компонентов игры
         window.clear();
         text.setString(PrintTitle(title, author));
         text.setPosition(0, 0);
@@ -251,13 +399,13 @@ int main()
         text.setString(PrintLetters(wordFromUser));
         text.setPosition(0, 350);
         window.draw(text);
-        text.setString(result);
+        text.setString(UsersWordAndHint);
         text.setPosition(0, 520);
         window.draw(text);
-
         sf::Text textSmall("", font, 25);
         textSmall.setColor(sf::Color(255, 69, 0));
 
+        // проверка на выигрыш/проигрыш и отображение дальнейших указаний
         if (won)
         {
             textSmall.setString("Press enter to start new game...");
@@ -270,7 +418,7 @@ int main()
             textSmall.setPosition(0, 730);
             window.draw(textSmall);
         }
-        if (tries == 10) StartNewGame(tries, won, result, wordFromUser, wordAndHint);
+        if (tries == 10) StartNewGame(tries, won, UsersWordAndHint, wordFromUser, wordAndHint);
 
         window.display();
     }
