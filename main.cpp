c66d2dcf69c278e0a3ce14237311c2b06371fefc
annanasnas <avsyurakshina@miem.hse.ru>
2022-06-18 01:27:58 +0300

    do clang-format

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <ctime>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <ctime>
-#include <SFML/Graphics.hpp>
 using namespace std;
 
 /*
@@ -22,41 +22,34 @@ using namespace std;
  * слово и подсказку, соответственно.
  *
  * */
-class Database
-{
-    string wordToGuess;
-    string hint;
+class Database {
+  string wordToGuess;
+  string hint;
+
 public:
-    Database(string filePath)
-    {
-        string word;
-        vector<string> v;
-        ifstream reader(filePath);
-        if (reader.is_open())
-        {
-            while (std::getline(reader, word)) v.push_back(word);
-            int randomLine = rand() % v.size();
-            string wordAndHint = v.at(randomLine);
-            int index = 0;
-            for(int i = wordAndHint.length() - 1; i >= 0; --i) {
-                if (wordAndHint[i] == ' ') {
-                    index = i;
-                    break;
-                }
-            }
-            hint = wordAndHint.substr(0, index);
-            wordToGuess = wordAndHint.erase(0,wordAndHint.find_last_of(' ')+1);
-            reader.close();
+  Database(string filePath) {
+    string word;
+    vector<string> v;
+    ifstream reader(filePath);
+    if (reader.is_open()) {
+      while (std::getline(reader, word))
+        v.push_back(word);
+      int randomLine = rand() % v.size();
+      string wordAndHint = v.at(randomLine);
+      int index = 0;
+      for (int i = wordAndHint.length() - 1; i >= 0; --i) {
+        if (wordAndHint[i] == ' ') {
+          index = i;
+          break;
         }
+      }
+      hint = wordAndHint.substr(0, index);
+      wordToGuess = wordAndHint.erase(0, wordAndHint.find_last_of(' ') + 1);
+      reader.close();
     }
-    string& returnWord()
-    {
-        return wordToGuess;
-    }
-    string& returnHint()
-    {
-        return hint;
-    }
+  }
+  string &returnWord() { return wordToGuess; }
+  string &returnHint() { return hint; }
 };
 
 /*
@@ -71,14 +64,14 @@ public:
  * @return строку вида: "|  message  |",
  *
  */
-string MessageInTheMiddle(string message, int lineLength)
-{
-    for (int i = message.length(); i < lineLength - 2; i++)
-    {
-        if (i % 2 == 0) message = " " + message;
-        else message = message + " ";
-    }
-    return "|" + message + "|";
+string MessageInTheMiddle(string message, int lineLength) {
+  for (int i = message.length(); i < lineLength - 2; i++) {
+    if (i % 2 == 0)
+      message = " " + message;
+    else
+      message = message + " ";
+  }
+  return "|" + message + "|";
 }
 
 /*
@@ -88,28 +81,27 @@ string MessageInTheMiddle(string message, int lineLength)
  *
  * @param content - ЧТО будет отрисовано
  * @param printTop - если true, то будет отрисована верхняя горизонтальная линия
- * @param printBottom - если true, то будет отрисована нижняя горизонтальная линия
+ * @param printBottom - если true, то будет отрисована нижняя горизонтальная
+ * линия
  * @return оцентрированный контент с/без верхней и/или нижней линии,
  * в зависимости от значений printTop и PrintBottom
  *
  */
-string PrintMessage(string content, bool printTop = true, bool printBottom = true)
-{
-    string result;
-    string line = "+---------------------------------+";
-    if (printTop)
-    {
-        result += line;
-        result += "\n";
-    }
-    result += MessageInTheMiddle(content, line.length());
+string PrintMessage(string content, bool printTop = true,
+                    bool printBottom = true) {
+  string result;
+  string line = "+---------------------------------+";
+  if (printTop) {
+    result += line;
     result += "\n";
-    if (printBottom)
-    {
-        result += line;
-        result += "\n";
-    }
-    return result;
+  }
+  result += MessageInTheMiddle(content, line.length());
+  result += "\n";
+  if (printBottom) {
+    result += line;
+    result += "\n";
+  }
+  return result;
 }
 
 /*
@@ -123,12 +115,11 @@ string PrintMessage(string content, bool printTop = true, bool printBottom = tru
  * и с верхней и нижней горизонтальной линией
  *
  */
-string PrintTitle(string title = "Hangman", string author = "annanasnas")
-{
-    string result;
-    result += PrintMessage(title, true, false);
-    result += PrintMessage("by " + author, false, true);
-    return result;
+string PrintTitle(string title = "Hangman", string author = "annanasnas") {
+  string result;
+  result += PrintMessage(title, true, false);
+  result += PrintMessage("by " + author, false, true);
+  return result;
 }
 
 /*
@@ -141,49 +132,48 @@ string PrintTitle(string title = "Hangman", string author = "annanasnas")
  * @return строка с нарисованным hang man
  *
  */
-string DrawHangman(int guessCount)
-{
-    string result;
-    if (guessCount >= 1)
-        result += PrintMessage("|", false, false);
-    else
-        result += PrintMessage("", false, false);
+string DrawHangman(int guessCount) {
+  string result;
+  if (guessCount >= 1)
+    result += PrintMessage("|", false, false);
+  else
+    result += PrintMessage("", false, false);
 
-    if (guessCount >= 2)
-        result += PrintMessage("|", false, false);
-    else
-        result += PrintMessage("", false, false);
+  if (guessCount >= 2)
+    result += PrintMessage("|", false, false);
+  else
+    result += PrintMessage("", false, false);
 
-    if (guessCount >= 3)
-        result += PrintMessage("O", false, false);
-    else
-        result += PrintMessage("", false, false);
+  if (guessCount >= 3)
+    result += PrintMessage("O", false, false);
+  else
+    result += PrintMessage("", false, false);
 
-    if (guessCount == 4)
-        result += PrintMessage("/  ", false, false);
+  if (guessCount == 4)
+    result += PrintMessage("/  ", false, false);
 
-    if (guessCount == 5)
-        result += PrintMessage("/| ", false, false);
+  if (guessCount == 5)
+    result += PrintMessage("/| ", false, false);
 
-    if (guessCount >= 6)
-        result += PrintMessage("/|\\", false, false);
-    else
-        result += PrintMessage("", false, false);
+  if (guessCount >= 6)
+    result += PrintMessage("/|\\", false, false);
+  else
+    result += PrintMessage("", false, false);
 
-    if (guessCount >= 7)
-        result += PrintMessage("|", false, false);
-    else
-        result += PrintMessage("", false, false);
+  if (guessCount >= 7)
+    result += PrintMessage("|", false, false);
+  else
+    result += PrintMessage("", false, false);
 
-    if (guessCount == 8)
-        result += PrintMessage("/  ", false, false);
+  if (guessCount == 8)
+    result += PrintMessage("/  ", false, false);
 
-    if (guessCount >= 9)
-        result += PrintMessage("/ \\", false, false);
-    else
-        result += PrintMessage("", false, false);
+  if (guessCount >= 9)
+    result += PrintMessage("/ \\", false, false);
+  else
+    result += PrintMessage("", false, false);
 
-    return result;
+  return result;
 }
 
 /*
@@ -198,21 +188,18 @@ string DrawHangman(int guessCount)
  * кроме тех, что есть в wordFromUser
  *
  */
-string PrintAvailableLetters(string wordFromUser, char from, char to)
-{
-    string result;
-    string availableLetters;
-    for (char i = from; i <= to; i++)
-    {
-        if (wordFromUser.find(i) == string::npos)
-        {
-            availableLetters += i;
-            availableLetters += " ";
-        }
-        else availableLetters += "  ";
-    }
-    result += PrintMessage(availableLetters, false, false);
-    return result;
+string PrintAvailableLetters(string wordFromUser, char from, char to) {
+  string result;
+  string availableLetters;
+  for (char i = from; i <= to; i++) {
+    if (wordFromUser.find(i) == string::npos) {
+      availableLetters += i;
+      availableLetters += " ";
+    } else
+      availableLetters += "  ";
+  }
+  result += PrintMessage(availableLetters, false, false);
+  return result;
 }
 
 /*
@@ -226,13 +213,12 @@ string PrintAvailableLetters(string wordFromUser, char from, char to)
  * строк без букв, которые есть в wordFromUser
  *
  */
-string PrintLetters(string wordFromUser)
-{
-    string result;
-    result += PrintMessage("Available letters");
-    result += PrintAvailableLetters(wordFromUser, 'A', 'M');
-    result += PrintAvailableLetters(wordFromUser,'N', 'Z');
-    return result;
+string PrintLetters(string wordFromUser) {
+  string result;
+  result += PrintMessage("Available letters");
+  result += PrintAvailableLetters(wordFromUser, 'A', 'M');
+  result += PrintAvailableLetters(wordFromUser, 'N', 'Z');
+  return result;
 }
 
 /*
@@ -258,28 +244,24 @@ string PrintLetters(string wordFromUser)
  * отгаданная буква
  *
  */
-void PrintWordAndCheckWin(string wordToGuess, string wordFromUser, string hint, bool &won, string &UsersWordAndHint)
-{
-    bool realWon = true;
-    string wordDisplayed;
-    UsersWordAndHint = "";
-    for (int i = 0; i < wordToGuess.length(); i++)
-    {
-        if (wordFromUser.find(wordToGuess[i]) == string::npos)
-        {
-            wordDisplayed += "_ ";
-            realWon = false;
-        }
-        else
-        {
-            wordDisplayed += wordToGuess[i];
-            wordDisplayed += " ";
-        }
+void PrintWordAndCheckWin(string wordToGuess, string wordFromUser, string hint,
+                          bool &won, string &UsersWordAndHint) {
+  bool realWon = true;
+  string wordDisplayed;
+  UsersWordAndHint = "";
+  for (int i = 0; i < wordToGuess.length(); i++) {
+    if (wordFromUser.find(wordToGuess[i]) == string::npos) {
+      wordDisplayed += "_ ";
+      realWon = false;
+    } else {
+      wordDisplayed += wordToGuess[i];
+      wordDisplayed += " ";
     }
-    UsersWordAndHint += PrintMessage("Hint", true, false);
-    UsersWordAndHint += PrintMessage(hint, false, true);
-    UsersWordAndHint += PrintMessage(wordDisplayed, false);
-    won = realWon;
+  }
+  UsersWordAndHint += PrintMessage("Hint", true, false);
+  UsersWordAndHint += PrintMessage(hint, false, true);
+  UsersWordAndHint += PrintMessage(wordDisplayed, false);
+  won = realWon;
 }
 
 /*
@@ -296,14 +278,13 @@ void PrintWordAndCheckWin(string wordToGuess, string wordFromUser, string hint, 
  * игроку засчитывается +1 попытка.
  *
  */
-int TriesLeft(string wordToGuess, string wordFromUser)
-{
-    int error = 0;
-    for (int i = 0; i < wordFromUser.length(); i++)
-    {
-        if (wordToGuess.find(wordFromUser[i]) == string::npos) error++;
-    }
-    return error;
+int TriesLeft(string wordToGuess, string wordFromUser) {
+  int error = 0;
+  for (int i = 0; i < wordFromUser.length(); i++) {
+    if (wordToGuess.find(wordFromUser[i]) == string::npos)
+      error++;
+  }
+  return error;
 }
 
 /*
@@ -320,14 +301,15 @@ int TriesLeft(string wordToGuess, string wordFromUser)
  * Изменяем значения всех переменных как в начале игры
  *
  */
-void StartNewGame(int &tries, bool &won, string &UsersWordAndHint, string &wordFromUser, Database &wordAndHint)
-{
-    wordAndHint = Database( "words.txt");
-    tries = 0;
-    won = false;
-    UsersWordAndHint = "";
-    wordFromUser = "";
-    PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint(), won, UsersWordAndHint);
+void StartNewGame(int &tries, bool &won, string &UsersWordAndHint,
+                  string &wordFromUser, Database &wordAndHint) {
+  wordAndHint = Database("words.txt");
+  tries = 0;
+  won = false;
+  UsersWordAndHint = "";
+  wordFromUser = "";
+  PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser,
+                       wordAndHint.returnHint(), won, UsersWordAndHint);
 }
 
 /*
@@ -336,92 +318,93 @@ void StartNewGame(int &tries, bool &won, string &UsersWordAndHint, string &wordF
  * @brief основная логика игры
  *
  */
-int main()
-{
-    srand(time(0));
-    string title = "Hangman";
-    string author = "annanasnas";
+int main() {
+  srand(time(0));
+  string title = "Hangman";
+  string author = "annanasnas";
 
-    // инициализация основных для игры переменных
-    Database wordAndHint = Database( "words.txt");;
-    int tries = 0;
-    bool won = false;
-    string UsersWordAndHint;
-    string wordFromUser;
+  // инициализация основных для игры переменных
+  Database wordAndHint = Database("words.txt");
+  ;
+  int tries = 0;
+  bool won = false;
+  string UsersWordAndHint;
+  string wordFromUser;
 
-    // подготовка интерфейса игры
-    sf::RenderWindow window(sf::VideoMode(600, 780), "Hangman", sf::Style::Close | sf::Style::Titlebar);
-    sf::Font font;
-    font.loadFromFile("consolas.ttf");
-    sf::Text text("", font, 30);
+  // подготовка интерфейса игры
+  sf::RenderWindow window(sf::VideoMode(600, 780), "Hangman",
+                          sf::Style::Close | sf::Style::Titlebar);
+  sf::Font font;
+  font.loadFromFile("consolas.ttf");
+  sf::Text text("", font, 30);
 
-    // отображение подсказки и числа букв, что нужно ввести пользователю
-    PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint(), won, UsersWordAndHint);
+  // отображение подсказки и числа букв, что нужно ввести пользователю
+  PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser,
+                       wordAndHint.returnHint(), won, UsersWordAndHint);
 
-    while (window.isOpen())
-    {
-        char letterFromUser;
-        sf::Event event;
-        // обработчик событий
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            if (event.type == sf::Event::TextEntered)
-            {
-                // если введена буква
-                if (event.text.unicode < 128)
-                {
-                    // пока пользователь не выиграл, можно вводить буквы
-                    if (!won)
-                    {
-                        letterFromUser = toupper(static_cast<char>(event.text.unicode));
-                        if (wordFromUser.find(letterFromUser) == string::npos) wordFromUser += letterFromUser;
-                        tries = TriesLeft(wordAndHint.returnWord(), wordFromUser);
-                        PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser, wordAndHint.returnHint(), won, UsersWordAndHint);
-                    }
-                }
-                // если пользователь выиграл, он должен нажать "enter" и начать новую игру
-                if (event.text.unicode == 10)
-                {
-                    if (won) StartNewGame(tries, won, UsersWordAndHint, wordFromUser, wordAndHint);
-                }
-            }
+  while (window.isOpen()) {
+    char letterFromUser;
+    sf::Event event;
+    // обработчик событий
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+      if (event.type == sf::Event::TextEntered) {
+        // если введена буква
+        if (event.text.unicode < 128) {
+          // пока пользователь не выиграл, можно вводить буквы
+          if (!won) {
+            letterFromUser = toupper(static_cast<char>(event.text.unicode));
+            if (wordFromUser.find(letterFromUser) == string::npos)
+              wordFromUser += letterFromUser;
+            tries = TriesLeft(wordAndHint.returnWord(), wordFromUser);
+            PrintWordAndCheckWin(wordAndHint.returnWord(), wordFromUser,
+                                 wordAndHint.returnHint(), won,
+                                 UsersWordAndHint);
+          }
         }
-        // отрисовка основных компонентов игры
-        window.clear();
-        text.setString(PrintTitle(title, author));
-        text.setPosition(0, 0);
-        window.draw(text);
-        text.setString(DrawHangman(tries));
-        text.setPosition(0, 140);
-        window.draw(text);
-        text.setString(PrintLetters(wordFromUser));
-        text.setPosition(0, 350);
-        window.draw(text);
-        text.setString(UsersWordAndHint);
-        text.setPosition(0, 520);
-        window.draw(text);
-        sf::Text textSmall("", font, 25);
-        textSmall.setColor(sf::Color(255, 69, 0));
-
-        // проверка на выигрыш/проигрыш и отображение дальнейших указаний
-        if (won)
-        {
-            textSmall.setString("Press enter to start new game...");
-            textSmall.setPosition(0, 730);
-            window.draw(textSmall);
+        // если пользователь выиграл, он должен нажать "enter" и начать новую
+        // игру
+        if (event.text.unicode == 10) {
+          if (won)
+            StartNewGame(tries, won, UsersWordAndHint, wordFromUser,
+                         wordAndHint);
         }
-        if (tries == 9)
-        {
-            textSmall.setString("Your last attempt");
-            textSmall.setPosition(0, 730);
-            window.draw(textSmall);
-        }
-        if (tries == 10) StartNewGame(tries, won, UsersWordAndHint, wordFromUser, wordAndHint);
-
-        window.display();
+      }
     }
+    // отрисовка основных компонентов игры
+    window.clear();
+    text.setString(PrintTitle(title, author));
+    text.setPosition(0, 0);
+    window.draw(text);
+    text.setString(DrawHangman(tries));
+    text.setPosition(0, 140);
+    window.draw(text);
+    text.setString(PrintLetters(wordFromUser));
+    text.setPosition(0, 350);
+    window.draw(text);
+    text.setString(UsersWordAndHint);
+    text.setPosition(0, 520);
+    window.draw(text);
+    sf::Text textSmall("", font, 25);
+    textSmall.setColor(sf::Color(255, 69, 0));
 
-    return 0;
+    // проверка на выигрыш/проигрыш и отображение дальнейших указаний
+    if (won) {
+      textSmall.setString("Press enter to start new game...");
+      textSmall.setPosition(0, 730);
+      window.draw(textSmall);
+    }
+    if (tries == 9) {
+      textSmall.setString("Your last attempt");
+      textSmall.setPosition(0, 730);
+      window.draw(textSmall);
+    }
+    if (tries == 10)
+      StartNewGame(tries, won, UsersWordAndHint, wordFromUser, wordAndHint);
+
+    window.display();
+  }
+
+  return 0;
 }
